515c4dced70f1cf5d8c1f9a50472b761c2483869
Denis Onder <denis_onder@protonmail.com>
2019-10-18 00:29:00 +0200

    Working version numero one

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 > gmem's gonna hate us
 > ~Autumn October 17th 2019
+
+Stolen from [http://www.computercraft.info/forums2/index.php?/topic/10056-room-maker-square-3-high/](http://www.computercraft.info/forums2/index.php?/topic/10056-room-maker-square-3-high/)
--- main.lua
+++ main.lua
@@ -1,40 +1,130 @@
-local passes = 0
-
-local function Start()
-	StartDigging()
+local tArgs = {...}
+if #tArgs ~= 1 then
+    print("Usage: makeroom <size>")
+    return
+end
+-- Mine in a spiral pattern
+local size = tonumber(tArgs[1])
+if size < 1 then
+    print("Room size must be positive")
+    return
 end
 
-local function Dig(num)
-	for i = 1, num do
-		-- If a block exists:
-		if turtle.detect() then
-			turtle.dig()
-			turtle.forward()
-		else
-			-- No block
-			turtle.forward();
-			Dig(num - 1)
-		end
-	end
+local collected = 0
+local function collect()
+    collected = collected + 1
+    if math.fmod(collected, 25) == 0 then
+        print("Mined " .. collected .. " items.")
+    end
+end
+local function tryDig()
+    while turtle.detect() do
+        if turtle.dig() then
+            collect()
+            sleep(0.5)
+        else
+            return false
+        end
+    end
+    return true
 end
 
--- Startup function
-function StartDigging()
-	write("Width: ")
-	local width = tonumber(read())
-	while width ~= passes do
-		Dig(width)
-		passes = passes	+ 1
-		turtle.turnRight()
-		Dig(1)
-		turtle.turnRight()
-	end
-	write("Keep digging? (Y/n)")
-	local Answer = read()
-	if(Answer:lower() == "y") then 
-		Start()
-	end
-	print("Done!")
+local function tryDigUp()
+    while turtle.detectUp() do
+        if turtle.digUp() then
+            collect()
+            sleep(0.5)
+        else
+            return false
+        end
+    end
+    return true
 end
 
-Start()
+local function refuel()
+    local fuelLevel = turtle.getFuelLevel()
+    if fuelLevel == "unlimited" or fuelLevel > 0 then return end
+
+    local function tryRefuel()
+        for n = 1, 16 do
+            if turtle.getItemCount(n) > 0 then
+                turtle.select(n)
+                if turtle.refuel(1) then
+                    turtle.select(1)
+                    return true
+                end
+            end
+        end
+        turtle.select(1)
+        return false
+    end
+
+    if not tryRefuel() then
+        print("Add more fuel to continue.")
+        while not tryRefuel() do sleep(1) end
+        print("Resuming Room.")
+    end
+end
+
+local function tryUp()
+    refuel()
+    while not turtle.up() do
+        if turtle.detectUp() then
+            if not tryDigUp() then return false end
+        elseif turtle.attackUp() then
+            collect()
+        else
+            sleep(0.5)
+        end
+    end
+    return true
+end
+
+local function tryForward()
+    refuel()
+    while not turtle.forward() do
+        if turtle.detect() then
+            if not tryDig() then return false end
+        elseif turtle.attack() then
+            collect()
+        else
+            sleep(0.5)
+        end
+    end
+    return true
+end
+
+local function channelForward(length)
+    refuel()
+    for i = 1, length do
+        tryDigUp()
+        tryForward()
+    end
+    return true
+end
+
+local function digLoop()
+    local n = 1
+    while n < size do
+        channelForward(n)
+        turtle.turnRight()
+        channelForward(n)
+        turtle.turnRight()
+        n = n + 1
+        if (n >= size) then
+            write("Would you like this any bigger? (Y/n)")
+            if (read():lower() == "y") then
+                write("How much bigger?")
+                size = tonumber(read())
+                n = 1
+            end
+        end
+    end
+end
+
+print("Making Room...")
+digLoop()
+channelForward(n - 1)
+tryDigUp()
+print("Room complete.")
+print("Mined " .. collected .. " items total.")
